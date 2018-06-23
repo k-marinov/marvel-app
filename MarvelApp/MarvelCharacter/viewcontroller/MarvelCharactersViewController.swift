0bcaad9b5f2b1edaba850f9de65eb4c31a7877b0
keran.marinov <linxmap@gmail.com>
2018-06-23 21:25:49 +0100

    activity spinner added

--- MarvelApp/MarvelCharacter/viewcontroller/MarvelCharactersViewController.swift
+++ MarvelApp/MarvelCharacter/viewcontroller/MarvelCharactersViewController.swift
@@ -8,6 +8,7 @@ class MarvelCharactersViewController: UIViewController, ModelableViewController 
     }()
     var viewModel: ViewModel!
 
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
     override func viewDidLoad() {
         super.viewDidLoad()
         setUp()
@@ -23,16 +24,18 @@ class MarvelCharactersViewController: UIViewController, ModelableViewController 
         tableView.registerCellNib(with: MarvelCharacterCell.identifier)
         tableView.dataSource = marvelCharactersViewModel.dataSource
         tableView.delegate = marvelCharactersViewModel.delegate
+        tableView.tableFooterView = UIView(frame: CGRect.zero)
     }
 
     private func loadContent() {
-        marvelCharactersViewModel.loadAllMarvelCharacters(with: MarvelCharactersRequest(), onStarted: {
-
-        }, onCompleted: {
-            self.tableView.reloadData()
-        }) {
-
-        }
+        marvelCharactersViewModel.loadAllMarvelCharacters(with: MarvelCharactersRequest(), onStarted: {[weak self] in
+            self?.activityIndicatorView.startAnimating()
+        }, onCompleted: { [weak self] in
+            self?.activityIndicatorView.stopAnimating()
+            self?.tableView.reloadData()
+        }, onError: { [weak self] in
+            self?.activityIndicatorView.stopAnimating()
+        })
     }
 
     private func setUpNavigationBarTitle() {
